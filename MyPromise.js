7acd1459df4455ed064c8d3c0bc852b59d1084cb
cary <CaryWill.K@gmail.com>
2021-09-27 23:05:48 +0800

    fix: 777+ 报错

--- MyPromise.js
+++ MyPromise.js
@@ -1,8 +1,8 @@
 const State = {
   PENDING: 0,
   FULFILLED: 1,
-  REJECTED: 2
-}
+  REJECTED: 2,
+};
 
 function MyPromise(executor) {
   this.state = State.PENDING;
@@ -12,101 +12,105 @@ function MyPromise(executor) {
   this.onRejectedCallbacks = [];
 
   const resolve = (val) => {
+    if (!this.state === State.PENDING) return;
+
     this.state = State.FULFILLED;
     this.value = val;
 
-    this.onFulfilledCallbacks.forEach(callback => {
+    this.onFulfilledCallbacks.forEach((callback) => {
       // 微任务(不过此处实现的方式是宏任务)
       setTimeout(() => {
         // 因为 then(res => callback(res)) 所以 callback 的参数是 this.value
         // 而且每次执行 callback 会改变 this.value 这样可以将上一个 callback 的值传递给下一个
         callback(this.value);
       }, 0);
-    })
-  }
+    });
+  };
 
   const reject = (reason) => {
+    if (!this.state === State.PENDING) return;
+
     this.state = State.FULFILLED;
     this.reason = reason;
 
-    this.onRejectedCallbacks.forEach(callback => {
+    this.onRejectedCallbacks.forEach((callback) => {
       // 微任务(不过此处实现的方式是宏任务)
       setTimeout(() => {
         callback(this.reason);
       }, 0);
-    })
-  }
+    });
+  };
 
   // executor 入参乱写导致报错
   try {
-    executor(resolve, reject)
+    executor(resolve, reject);
   } catch (error) {
     reject(error);
   }
 }
 
-function resolvePromise(value, resolve, reject) {
-  if (value instanceof MyPromise(executor)) {
-    value.then(res => {
-      if (res instanceof MyPromise) {
-        resolvePromise(res, resolve, reject);
-      } else {
-        resolve(res);
+function resolvePromise(promise, x, resolve, reject) {
+  // FIXME:规范上这么说，但是什么场景下会相同呢
+  if (promise === x) {
+    return reject(new TypeError("promise and value refer to the same object."));
+  }
+
+  if (x instanceof MyPromise) {
+    x.then(
+      (res) => {
+        if (res instanceof MyPromise) {
+          resolvePromise(promise, res, resolve, reject);
+        } else {
+          resolve(res);
+        }
+      },
+      (error) => {
+        if (error instanceof MyPromise) {
+          resolvePromise(promise, res, resolve, reject);
+        } else {
+          reject(error);
+        }
       }
-    }, error => {
-      if (error instanceof MyPromise) {
-        resolvePromise(res, resolve, reject);
+    );
+  } else if (["object", "function"].includes(typeof x)) {
+    // 如果是 thenable 的话
+    try {
+      if (typeof x.then === "function") {
+        x.then(
+          (y) => resolvePromise(promise, y, resolve, reject),
+          (r) => reject(r)
+        );
       } else {
-        reject(error);
+        resolve(x);
       }
-    })
-  } else if (value.then) {
-    // 如果是 thenable 的话做类似处理
-    // TODO:
+    } catch (error) {
+      reject(error);
+    }
   } else {
-    resolve(value);
+    resolve(x);
   }
 }
 
-MyPromise.prototype.then = function then(onFulfilled, onRejected) {
-  if (this.state === State.PENDING) {
-    const promise = new MyPromise((resolve, reject) => {
-      // 包一层然后加入到 callback list 中等待 this 被 settle
-      // 包一层是因为有可能 onFulfilled() 返回 promise
-      this.onFulfilledCallbacks.push((value) => {
-        const _value = onFulfilled(value);
-        if (_value instanceof MyPromise) {
-          resolvePromise(_value, resolve, reject);
-        } else {
-          resolve(_value);
-        }
-      })
-
-      this.onRejectedCallbacks.push((reason) => {
-        const _value = onFulfilled(reason);
-        if (_value instanceof MyPromise) {
-          resolvePromise(_value, resolve, reject);
-        } else {
-          resolve(_value);
-        }
-      })
-    })
-    // 直接返回以便 promise chaining
-    return promise;
-  }
-
+MyPromise.prototype.then = function then(
+  onFulfilled = (v) => v,
+  onRejected = (v) => v
+) {
   if (this.state === State.FULFILLED) {
     const promise = new MyPromise((resolve, reject) => {
       // 因为 fulfilled 了，所以直接加入到微任务中即可
       setTimeout(() => {
-        const _value = onFulfilled(value);
-        if (_value instanceof MyPromise) {
-          resolvePromise(_value, resolve, reject);
-        } else {
-          resolve(_value);
+        try {
+          const _value = onFulfilled(this.value);
+          if (_value instanceof MyPromise) {
+            resolvePromise(promise, _value, resolve, reject);
+          } else {
+            resolve(_value);
+          }
+        } catch (error) {
+          reject(error);
         }
-      }, 0)
-    })
+      }, 0);
+    });
     return promise;
   }
 
@@ -114,17 +118,55 @@ MyPromise.prototype.then = function then(onFulfilled, onRejected) {
     const promise = new MyPromise((resolve, reject) => {
       // 因为 fulfilled 了，所以直接加入到微任务中即可
       setTimeout(() => {
-        const _value = onRejected(value);
-        if (_value instanceof MyPromise) {
-          resolvePromise(_value, resolve, reject);
-        } else {
-          resolve(_value);
+        try {
+          const _value = onRejected(this.value);
+          if (_value instanceof MyPromise) {
+            resolvePromise(promise, _value, resolve, reject);
+          } else {
+            resolve(_value);
+          }
+        } catch (error) {
+          reject(error);
         }
-      }, 0)
-    })
+      }, 0);
+    });
     return promise;
   }
-}
+
+  if (this.state === State.PENDING) {
+    const promise = new MyPromise((resolve, reject) => {
+      // 包一层然后加入到 callback list 中等待 this 被 settle
+      // 包一层是因为有可能 onFulfilled() 返回 promise
+      this.onFulfilledCallbacks.push((value) => {
+        try {
+          const _value = onFulfilled(value);
+          if (_value instanceof MyPromise) {
+            resolvePromise(promise, _value, resolve, reject);
+          } else {
+            resolve(_value);
+          }
+        } catch (error) {
+          reject(error);
+        }
+      });
+
+      this.onRejectedCallbacks.push((reason) => {
+        try {
+          const _value = onFulfilled(reason);
+          if (_value instanceof MyPromise) {
+            resolvePromise(promise, _value, resolve, reject);
+          } else {
+            resolve(_value);
+          }
+        } catch (error) {
+          reject(error);
+        }
+      });
+    });
+    // 直接返回以便 promise chaining
+    return promise;
+  }
+};
 
 MyPromise.deferred = function () {
   var result = {};
@@ -134,6 +176,6 @@ MyPromise.deferred = function () {
   });
 
   return result;
-}
+};
 
 module.exports = MyPromise;
